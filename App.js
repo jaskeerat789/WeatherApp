998e600a12787e23a0b365e08dd6d7a2d44c70cd
jaskeerat789 <jaskee789@gmail.com>
2019-01-23 01:55:34 +0530

    added the ivalid city alert

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View,TextInput,Button, } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button,ActivityIndicator,Keyboard,Alert} from 'react-native';
 import {Ionicons} from '@expo/vector-icons'
 export default class App extends React.Component {
   constructor(props)
   {
     super(props)
     this.state={
-      isLoading:'true',
+      isLoading:false,
       data:null,
-      temp:null,
+      temp:'',
       city:null,
+      
       iconlist:[
         '',
         'ios-sunny',
@@ -28,9 +29,10 @@ export default class App extends React.Component {
   }
 
   press=()=>{
+    Keyboard.dismiss()
     console.log('in the press');
     
-    this.setState({city:this.state.temp},()=>{
+    this.setState({city:this.state.temp,isLoading:true,temp:''},()=>{
         console.log("in the set state",this.state.city);
         if(this.state.city)
         {
@@ -38,32 +40,105 @@ export default class App extends React.Component {
         return fetch(url)
         .then((res)=>res.json())
         .then((json)=>{
+          console.log(json)
+          if(json.cod!="404")
+          {
           let icon_number=parseInt(json.weather[0].icon.substring(0,2));
-          this.setState({
-          data:(json.main.temp-273).toFixed(0).toString(),
-          description:json.weather[0].description,
-          iconname:json.weather[0].icon,
-          icon:this.state.iconlist[icon_number]})})
-        .then(()=>console.log(this.state))
+          this.setState(
+            {
+            json:json,
+            code:json.cod,
+            data:(json.main.temp-273).toFixed(0).toString(),
+            description:json.weather[0].description,
+            iconname:json.weather[0].icon,
+            icon:this.state.iconlist[icon_number],
+            isLoading:false
+            }
+          )
+          }
+          else
+          {
+            this.setState({data:json.message,code:json.cod,isLoading:false})
+          }
+        })
+        .then(()=>console.log(this.state)
+        )
         .catch((e)=>console.log(e))
     }
   })
 }
 
+clear = () => {
+  this.textInputRef.clear();
+}
+check=()=>{
+  if(this.state.isLoading)
+  {
+    return(
+      <View style={{width:'100%',justifyContent:'center',alignItems:'center',height:300}}>
+      <ActivityIndicator size='large' color='white' />
+      </View>
+    )
+  }
+  else if(this.state.code=='404')
+  {
+      Alert.alert(this.state.data)
+      return(<View style={{width:'100%',justifyContent:'center',alignItems:'center',height:300}}>
+        <Text style={[styles.text,{marginTop:40}]}>{"WELCOME"}</Text>
+        <Ionicons style={{paddingTop:0,justifyContent:'center'}} name={this.state.icon} size={120} color='white'/>
+      </View>
+        
+      )
+  }
+  else if(this.state.code=='200')
+  {
+    return(
+      <View style={{width:'100%',justifyContent:'center',alignItems:'center',height:300}}>
+        <Text style={[styles.text,{marginTop:40}]}>{(this.state.json.name)}</Text>
+        <Ionicons style={{paddingTop:0,justifyContent:'center'}} name={this.state.icon} size={120} color='white'/>
+        <Text style={styles.text}>{(this.state.data)}°C</Text>
+        <Text style={{fontSize:20,color:'rgba(255,255,255,0.95)'}}>{(this.state.description)}</Text>
+      </View>
+    )    
+
+  }
+  else{
+    return(<View style={{width:'100%',justifyContent:'center',alignItems:'center',height:300}}>
+    <Text style={[styles.text,{marginTop:40}]}>{"WELCOME"}</Text>
+    <Ionicons style={{paddingTop:0,justifyContent:'center'}} name={this.state.icon} size={120} color='white'/>
+  </View>)
+  }
+}
+
   render() {
+    
     return (
       <View style={styles.container}>
-      {this.state.city?<Text style={[styles.text,{marginTop:40}]}>{(this.state.city)}</Text>:<Text style={[styles.text,{marginTop:40}]}>{"WELCOME"}</Text>}
+      {/* {this.state.isLoading?
+
+         <View style={{width:'100%',justifyContent:'center',alignItems:'center',height:300}}>
+         <ActivityIndicator size='large' color='white' />
+         </View>
+        :
+      (
+      <View style={{width:'100%',justifyContent:'center',alignItems:'center',height:300}}>
+      {this.state.data?<Text style={[styles.text,{marginTop:40}]}>{(this.state.json.name)}</Text>:<Text style={[styles.text,{marginTop:40}]}>{"WELCOME"}</Text>}
       <Ionicons style={{paddingTop:0,justifyContent:'center'}} name={this.state.icon} size={120} color='white'/>
       {this.state.data?<Text style={styles.text}>{(this.state.data)}°C</Text>:<Text style={styles.text}>{""}</Text>}
       {this.state.data?<Text style={{fontSize:20,color:'rgba(255,255,255,0.95)'}}>{(this.state.description)}</Text>:<Text >{""}</Text>}
-      
+      </View>
+      )
+      } */}
+      {this.check()}   
 
         <View style={styles.searchbar}>
         
         <TextInput placeholder="enter here" style={styles.textInput}
           onChangeText={(input)=>this.setState({temp:input})}
           underlineColorAndroid='rgba(0,0,0,0)'
+          clearTextOnFocus={true}
+          ref={ref => this.textInputRef = ref}
+          value={this.state.temp}
         />
         
         <Button style={styles.button} title="search"
